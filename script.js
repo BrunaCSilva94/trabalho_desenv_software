512816bb3e9cf43f122dab5a9c9b558b3f68ab3c
BrunaCSilva94 <bruhcosta10@gmail.com>
2025-05-19 23:44:42 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById('jsButton').addEventListener('click', function () {
+    document.getElementById('jsMessage').innerHTML = "O aprendizado é um presente que nos damos e o conhecimento é o laço que o envolve.";
+});
